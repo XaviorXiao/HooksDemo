b421bf76ce4b0c9c2d49bb07c35cba189540af2c
xiao <xuxiaoxiao@vmcshop.com>
2019-05-29 16:22:35 +0800

    suibian

--- HooksDemo/ViewController.m
+++ HooksDemo/ViewController.m
@@ -18,7 +18,21 @@
     [super viewDidLoad];
     
     //这里添加几行代码，用来测试
+    [self testing];
+    [self testing2];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)testing{
+    if ([self isKindOfClass:[ViewController class]]) {
+        
+    }
+}
+
+-(void)testing2{
+    if ([self isKindOfClass:[ViewController class]]) {
+        
+    }
 }
 
 
