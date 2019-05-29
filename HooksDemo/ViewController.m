b21d8683386e77105ca78dd3a4d8d4e88b8bf2b3
xiao <xuxiaoxiao@vmcshop.com>
2019-05-29 18:01:10 +0800

    add 5

--- HooksDemo/ViewController.m
+++ HooksDemo/ViewController.m
@@ -8,15 +8,18 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
 
 @end
 
+
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    
+
     //这里添加几行代码，用来测试
     [self testing];
     [self testing2];
@@ -24,21 +27,21 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
--(void)testing{
-               if ([self isKindOfClass:[ViewController class]]) {
-        
+- (void)testing
+{
+    if ([self isKindOfClass:[ViewController class]]) {
     }
 }
 
--(void)testing2{
-               if ([self isKindOfClass:[ViewController class]]) {
-        
+- (void)testing2
+{
+    if ([self isKindOfClass:[ViewController class]]) {
     }
 }
 
--(void)testing3{
-             if ([self isKindOfClass:[ViewController class]]) {
-        
+- (void)testing3
+{
+    if ([self isKindOfClass:[ViewController class]]) {
     }
 }
 
